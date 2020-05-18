c3faa5ab87ad2a58aed31290845fa9e2f715db6b
William Lockwood <jinkside@gmail.com>
2020-05-18 09:00:48 -0700

    Added ask_for_word

--- InterfaceHelpers.py
+++ InterfaceHelpers.py
@@ -1,3 +1,6 @@
+# TODO: Tell user if there is a default
+# TODO: Tell user if required
+# TODO: Previous two are mutually exclusive?
 def ask_for_int(query: str, required: bool = True, default: int = None,
                 accept_min=float("-inf"), accept_max=float("inf")):
     while True:
@@ -20,4 +23,27 @@ def ask_for_int(query: str, required: bool = True, default: int = None,
             print(f"Unable to parse '{user_input}' as an integer. Please enter a valid number.")
 
 
+# TODO: Look into parameter sets. Having required and default is weird.
+def ask_for_word(query: str, required: bool = True, allow_spaces=False, default: str = None, max_len=float("inf")):
+    while True:
+        acceptable_input = True
+        print()
+        user_input = input(f"{query}?\n")
 
+        if required and default:
+            raise Exception("ask_for_word was called with both a default value and required. Please don't.")
+
+        if required and len(user_input) < 1:
+            print(f"This is a required value. Please input something.")
+            acceptable_input = False
+
+        if allow_spaces and " " in user_input:
+            print(f"Spaces are not allowed, but '{user_input}' contains at least one space character.")
+            acceptable_input = False
+
+        if len(user_input) > max_len:
+            print(f"Your input of '{user_input}' is greater than the max length of {max_len:,}")
+            acceptable_input = False
+
+        if acceptable_input:
+            return user_input
